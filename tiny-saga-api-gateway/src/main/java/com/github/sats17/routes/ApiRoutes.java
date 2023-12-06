ea547e0fb357f3330ee5c19792ce9f4ee2d9ebe8
sats17 <satishkumbhar9999@gmail.com>
2023-12-06 22:50:23 +0530

    Initialized api gw with order ms

--- tiny-saga-api-gateway/src/main/java/com/github/sats17/routes/ApiRoutes.java
+++ tiny-saga-api-gateway/src/main/java/com/github/sats17/routes/ApiRoutes.java
@@ -1,5 +1,6 @@
 package com.github.sats17.routes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,14 +9,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApiRoutes {
 
+	@Value("${services.order.requestPath}")
+	private String orderRequestPath;
+
+	@Value("${services.order.downstreamPath}")
+	private String orderDownStreamPath;
+
+	@Value("${services.order.downstreamHost}")
+	private String orderDownStreamHost;
 
 	@Bean
 	RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
-					  .route("v1_create_order", r -> r.path("/v1/saga/order")
-							  .filters(f -> f.rewritePath("/v1/saga/order/(?<remaining>.*)", "/v1/api/order"))
-							  .uri("http:/localhost:8081"))
-					  		  .build();
+				.route("v1_create_order", r -> r.path("/v1/saga/order")
+						.filters(f -> f.rewritePath(orderRequestPath, orderDownStreamPath)).uri(orderDownStreamHost))
+				.build();
 	}
-	
+
 }
