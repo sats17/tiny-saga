ea6cbe4058159366bcc55bf1e9c0424deff008a9
sats17 <satishkumbhar9999@gmail.com>
2024-01-06 21:59:04 +0530

    Added payment proto

--- tiny-saga-payment-ms/src/main/java/com/github/sats17/payment/controller/PyamentApiController.java
+++ tiny-saga-payment-ms/src/main/java/com/github/sats17/payment/controller/PyamentApiController.java
@@ -34,23 +34,23 @@ public class PyamentApiController {
 		return "ok ok health from transaction";
 	}
 
-	@GetMapping("/dev/transactions")
-	public List<Transaction> getAllTransactions() {
-		AppUtils.printLog("Data present in transaction DB " + transactionRepository.count());
-		Iterable<Transaction> transactionIterable = transactionRepository.findAll();
-		return StreamSupport.stream(transactionIterable.spliterator(), false).toList();
-	}
+//	@GetMapping("/dev/transactions")
+//	public List<Transaction> getAllTransactions() {
+//		AppUtils.printLog("Data present in transaction DB " + transactionRepository.count());
+//		Iterable<Transaction> transactionIterable = transactionRepository.findAll();
+//		return StreamSupport.stream(transactionIterable.spliterator(), false).toList();
+//	}
 
-	@GetMapping("/dev/transactions/order")
-	public List<Transaction> getAllTransactionsForOrder(@RequestParam String orderId) {
-		AppUtils.printLog("Data present in transaction DB " + transactionRepository.count());
-		Iterable<Transaction> transactionIterable = transactionRepository.findAll();
-		return StreamSupport.stream(transactionIterable.spliterator(), false).filter(transaction -> {
-			return transaction.getOrderId().equals(orderId);
-		}).sorted((t1, t2) -> {
-			return t1.getTimestamp().compareTo(t2.getTimestamp());
-		}).toList();
-	}
+//	@GetMapping("/dev/transactions/order")
+//	public List<Transaction> getAllTransactionsForOrder(@RequestParam String orderId) {
+//		AppUtils.printLog("Data present in transaction DB " + transactionRepository.count());
+//		Iterable<Transaction> transactionIterable = transactionRepository.findAll();
+//		return StreamSupport.stream(transactionIterable.spliterator(), false).filter(transaction -> {
+//			return transaction.getOrderId().equals(orderId);
+//		}).sorted((t1, t2) -> {
+//			return t1.getTimestamp().compareTo(t2.getTimestamp());
+//		}).toList();
+//	}
 
 //	@PostMapping("/process")
 //	public List<Transaction> updatePayment(@RequestBody PaymentProcessRequest request) {
--- tiny-saga-payment-ms/src/main/java/com/github/sats17/payment/service/PaymentService.java
+++ tiny-saga-payment-ms/src/main/java/com/github/sats17/payment/service/PaymentService.java
@@ -2,17 +2,29 @@ package com.github.sats17.payment.service;
 
 import org.springframework.stereotype.Service;
 
+import com.github.sats17.grpc.payment.PaymentRequest;
+import com.github.sats17.grpc.payment.PaymentResponse;
+import com.github.sats17.grpc.payment.PaymentServiceGrpc;
 import com.github.sats17.payment.model.v2.PaymentProcessRequest;
 
 @Service
-public class PaymentService {
+public class PaymentService extends PaymentServiceGrpc.PaymentServiceImplBase {
 
 	public void performPayment(PaymentProcessRequest request) {
-		
+
 	}
-	
+
 	public void performRefund(PaymentProcessRequest request) {
-		
+
 	}
-	
+
+	@Override
+	public void processPayment(com.github.sats17.grpc.payment.PaymentRequest request,
+			io.grpc.stub.StreamObserver<com.github.sats17.grpc.payment.PaymentResponse> responseObserver) {
+		System.out.println(request.toString());
+		PaymentResponse rp = PaymentResponse.newBuilder().setMessage("From server").build();
+		responseObserver.onNext(rp);
+		responseObserver.onCompleted();
+	}
+
 }
