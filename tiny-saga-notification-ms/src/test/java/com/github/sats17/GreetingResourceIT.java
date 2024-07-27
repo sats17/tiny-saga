b574ed21d81fe7fa69d7f359c9882af92e67cdb0
sats17 <satishkumbhar9999@gmail.com>
2024-07-27 16:23:35 +0530

    Added notification ms initlaized code

---
+++ tiny-saga-notification-ms/src/test/java/com/github/sats17/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.github.sats17;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
